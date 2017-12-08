dd1366418ac07dd0ebae0814e7afa7235c9faed3
waukens <my4all@yandex.ru>
2017-12-08 22:03:28 +0300

    Create initDB and populateDB for meals

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,11 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS global_seq_meal;
 
 CREATE SEQUENCE global_seq START 100000;
+CREATE SEQUENCE global_seq_meal START 100000;
 
 CREATE TABLE users
 (
@@ -21,5 +24,16 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq_meal'),
+  date_time TIMESTAMP NOT NULL DEFAULT now(),
+  description VARCHAR NOT NULL,
+  calories INTEGER DEFAULT 2000 NOT NULL,
+  user_id INTEGER NOT NULL,
+  CONSTRAINT meals_idx UNIQUE (date_time, description, user_id),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE global_seq_meal RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +11,11 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, user_id) VALUES
+  ('breakfast_USER', 100000),
+  ('lunch_ADMIN', 100001),
+  ('supper_USER', 100000),
+  ('breakfast_ADMIN', 100001),
+  ('lunch_USER', 100000),
+  ('supper_ADMIN', 100001);
