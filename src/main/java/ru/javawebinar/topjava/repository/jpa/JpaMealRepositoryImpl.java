4ca3302d3ea22bdbf938f716edd9e41003df84dd
waukens <my4all@yandex.ru>
2017-12-18 23:33:27 +0300

    JPA Meal save

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
@@ -2,17 +2,37 @@ package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaMealRepositoryImpl implements MealRepository {
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (meal.isNew()) {
+            meal.setUser(em.find(User.class, userId));
+            em.persist(meal);
+            return meal;
+        } else {
+            if (meal.getUser().getId() == userId) {
+                return em.merge(meal);
+            } else {
+                return null;
+            }
+        }
     }
 
     @Override
