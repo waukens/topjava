f6f7d284c1d4ea62b360e82a193ce3530a9215e9
waukens <my4all@yandex.ru>
2018-01-01 22:42:28 +0300

    Create @Profiles JPA, JDBC, DATAJPA

---
+++ src/test/java/ru/javawebinar/topjava/service/JpaUserServiceTest.java
@@ -0,0 +1,57 @@
+package ru.javawebinar.topjava.service;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
+
+@ActiveProfiles(Profiles.JPA)
+public class JpaUserServiceTest extends UserServiceTest{
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    public void create() throws Exception {
+        super.create();
+    }
+
+    @Override
+    public void duplicateMailCreate() throws Exception {
+        super.duplicateMailCreate();
+    }
+
+    @Override
+    public void delete() throws Exception {
+        super.delete();
+    }
+
+    @Override
+    public void notFoundDelete() throws Exception {
+        super.notFoundDelete();
+    }
+
+    @Override
+    public void get() throws Exception {
+        super.get();
+    }
+
+    @Override
+    public void getNotFound() throws Exception {
+        super.getNotFound();
+    }
+
+    @Override
+    public void getByEmail() throws Exception {
+        super.getByEmail();
+    }
+
+    @Override
+    public void update() throws Exception {
+        super.update();
+    }
+
+    @Override
+    public void getAll() throws Exception {
+        super.getAll();
+    }
+}
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -36,7 +36,7 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
-public class MealServiceTest {
+public abstract class MealServiceTest {
     private static final Logger log = getLogger("result");
 
     private static StringBuilder results = new StringBuilder();
